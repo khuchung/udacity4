66b7f6461eca1fbd7c616f6e5f191b513437df05
khuchung <khuc.hung@decopac.com>
2022-09-05 12:38:31 +0700

    Update to deploy to AKS

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Variables
-resourceGroup="acdnd-c4-project"
-clusterName="udacity-cluster"
+resourceGroup="udacity-c4-exercise"
+clusterName="udacity-4-cluster"
 
 # Install aks cli
 echo "Installing AKS CLI"
